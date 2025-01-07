63b1b8016863af99910b146ffebddd1f6dc4159f
Alisson Hiosaki <92666141+AlissonHiosaki@users.noreply.github.com>
2025-01-06 21:41:37 -0300

    Add files via upload

---
+++ assets/js/loading.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const loadingScreen = document.getElementById("loading-screen");
+    const loginScreen = document.getElementById("login-screen");
+
+    // Simula um tempo de carregamento
+    setTimeout(() => {
+        loadingScreen.classList.add("hidden"); // Esconde a tela de loading
+        loginScreen.classList.remove("hidden"); // Mostra a tela de login
+    }, 5000); // Ajuste o tempo conforme necessário (em milissegundos)
+});
