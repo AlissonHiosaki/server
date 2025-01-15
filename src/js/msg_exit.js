4228608fb558435b90b567f7c7f91111e62c3bb2
Alisson Hiosaki <92666141+AlissonHiosaki@users.noreply.github.com>
2025-01-15 01:29:13 -0300

    Delete src directory

--- src/js/msg_exit.js
+++
@@ -1,9 +0,0 @@
-//funcao de proibir de editar url
-
-function preventUrlEdit() {
-    window.onbeforeunload = function() {
-        return 'Você está prestes a sair da página. Confirme se deseja sair.';
-    };
-}
-
-preventUrlEdit();
